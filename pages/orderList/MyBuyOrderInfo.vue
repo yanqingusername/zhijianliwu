900d7a9ec7b9d043bea7a8ee54385f38f9bb68e8
yangqing <yangqing@jiunidu.com>
2021-11-19 16:42:47 +0800

    update

--- pages/orderList/MyBuyOrderInfo.vue
+++ pages/orderList/MyBuyOrderInfo.vue
@@ -341,6 +341,29 @@
 			},
 			//  微信支付
 			submit: function(e) {
+				// 调用订阅消息
+				uni.requestSubscribeMessage({
+					tmplIds: ['CMWMOxVzHq2eI_F-Hit5U3tvGCaENXCAUQwII4N2hYo','KJaeMwRJkgFsPDzIv0zc2JCUDWyMlaIu-z5WhCVR_GE,hc4lSJBWenqNrQ5hqaRLU4n2E-mRBTvxl42aW_25yRs'],
+					success(res) {
+						let action = "add_wx_subscribe_log";
+						let controller = 'subscribe';
+						let memberid = uni.getStorageSync('id')
+						let data = JSON.stringify({
+							memberid: memberid,
+							template_id:"CMWMOxVzHq2eI_F-Hit5U3tvGCaENXCAUQwII4N2hYo,KJaeMwRJkgFsPDzIv0zc2JCUDWyMlaIu-z5WhCVR_GE,hc4lSJBWenqNrQ5hqaRLU4n2E-mRBTvxl42aW_25yRs"
+						});
+						
+						this.$utils.postNew(action,data,controller).then(res=>{
+							if(res.sta == 1){
+								
+							}
+						})
+					},
+					fail(res) {
+						
+					}
+				});
+				
 				let orderNumber = e.currentTarget.dataset.ordernumber;
 				// 接口地址
 				let action = 'get_buy_order_pay_info';
--- pages/shopping/must.vue
+++ pages/shopping/must.vue
@@ -383,14 +383,14 @@
 			forsubmit: function(e) {
 				// 调用订阅消息
 				uni.requestSubscribeMessage({
-					tmplIds: ['CMWMOxVzHq2eI_F-Hit5U3tvGCaENXCAUQwII4N2hYo','KJaeMwRJkgFsPDzIv0zc2JCUDWyMlaIu-z5WhCVR_GE'],
+					tmplIds: ['CMWMOxVzHq2eI_F-Hit5U3tvGCaENXCAUQwII4N2hYo','KJaeMwRJkgFsPDzIv0zc2JCUDWyMlaIu-z5WhCVR_GE,hc4lSJBWenqNrQ5hqaRLU4n2E-mRBTvxl42aW_25yRs'],
 					success(res) {
 						let action = "add_wx_subscribe_log";
 						let controller = 'subscribe';
 						let memberid = uni.getStorageSync('id')
 						let data = JSON.stringify({
 							memberid: memberid,
-							template_id:"CMWMOxVzHq2eI_F-Hit5U3tvGCaENXCAUQwII4N2hYo,KJaeMwRJkgFsPDzIv0zc2JCUDWyMlaIu-z5WhCVR_GE"
+							template_id:"CMWMOxVzHq2eI_F-Hit5U3tvGCaENXCAUQwII4N2hYo,KJaeMwRJkgFsPDzIv0zc2JCUDWyMlaIu-z5WhCVR_GE,hc4lSJBWenqNrQ5hqaRLU4n2E-mRBTvxl42aW_25yRs"
 						});
 						
 						this.$utils.postNew(action,data,controller).then(res=>{
