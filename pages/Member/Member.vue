6f7ebbc57c3b580091fa86a6d62a0752b983959f
yangqing <yangqing@jiunidu.com>
2022-01-04 17:14:19 +0800

    version 4.2.86

--- pages/Member/Member.vue
+++ pages/Member/Member.vue
@@ -76,7 +76,7 @@
 			}
 		},
 		onLoad: function(options) {
-			this.typestring = options.typestring || uni.getStorageSync("level_name")=='企业会员' ? 2 : 1;
+			this.typestring = options.typestring || (uni.getStorageSync("level_name")=='企业会员' ? 2 : 1);
 			if(this.typestring == 1){
 				uni.setNavigationBarTitle({
 					　　title: '会员详情'
